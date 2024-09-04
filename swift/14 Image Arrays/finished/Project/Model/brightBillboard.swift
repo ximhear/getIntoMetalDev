f8282b07735e68a47b259f506d50046c367c1642
C.H Lee <ximhear@gmail.com>
2024-09-05 02:14:10 +0900

    .

--- swift/14 Image Arrays/finished/Project/Model/brightBillboard.swift
+++ swift/14 Image Arrays/finished/Project/Model/brightBillboard.swift
@@ -8,7 +8,6 @@
 import Foundation
 
 class BrightBillboard {
-    
     var position: simd_float3
     var model: matrix_float4x4
     var color: vector_float3
@@ -17,31 +16,28 @@ class BrightBillboard {
     var pathRadius: Float
     var pathPhi: Float
     var angularVelocity: Float
-    
+
     init(position: simd_float3, color: vector_float3, rotation_center: vector_float3, pathRadius: Float, pathPhi: Float, angularVelocity: Float) {
-        
         self.position = position
         self.color = color
-        self.t = 0
-        self.rotationCenter = rotation_center
+        t = 0
+        rotationCenter = rotation_center
         self.pathRadius = pathRadius
         self.pathPhi = pathPhi
         self.angularVelocity = angularVelocity
-        self.model = Matrix44.create_identity()
-        
+        model = Matrix44.create_identity()
     }
-    
+
     func update(viewerPosition: simd_float3) {
-        
         position[0] = rotationCenter[0] + pathRadius * cos(t) * sin(pathPhi * .pi / 180.0)
         position[1] = rotationCenter[1] + pathRadius * sin(t) * sin(pathPhi * .pi / 180.0)
         position[2] = rotationCenter[2] + pathRadius * cos(pathPhi * .pi / 180.0)
-        
-        t += angularVelocity * 0.1;
+
+        t += angularVelocity * 0.1
         if t > (2.0 * .pi) {
             t -= 2.0 * .pi
         }
-        
+
         let selfToViewer: simd_float3 = viewerPosition - position
         let theta: Float = simd.atan2(selfToViewer[1], selfToViewer[0]) * 180.0 / .pi
         let horizontalDistance: Float = sqrtf(selfToViewer[0] * selfToViewer[0] + selfToViewer[1] * selfToViewer[1])
@@ -49,5 +45,4 @@ class BrightBillboard {
         model = Matrix44.create_from_rotation(eulers: [0, phi, theta])
         model = Matrix44.create_from_translation(translation: position) * model
     }
-    
 }
--- swift/14 Image Arrays/finished/Project/controller.swift
+++ swift/14 Image Arrays/finished/Project/controller.swift
@@ -12,40 +12,38 @@ struct Point2D {
 }
 
 class InputController {
-    static let controller: InputController = InputController()
+    static let controller: InputController = .init()
     var keysPressed: Set<GCKeyCode> = []
     var mousePressed: Bool = false
-    var mouseDelta: Point2D = Point2D(x: 0, y: 0)
-    
+    var mouseDelta: Point2D = .init(x: 0, y: 0)
+
     init() {
-        let center = NotificationCenter.default;
-        
+        let center = NotificationCenter.default
+
         center.addObserver(forName: .GCKeyboardDidConnect,
-                           object: nil, queue: nil
-        ) {
+                           object: nil, queue: nil)
+        {
             notification in
-                let keyboard = notification.object as? GCKeyboard
-                keyboard?.keyboardInput?.keyChangedHandler =
-                {_, _, keyCode, pressed in
-                    if pressed {
-                        self.keysPressed.insert(keyCode)
-                    }
-                    else {
-                        self.keysPressed.remove(keyCode)
-                    }
+            let keyboard = notification.object as? GCKeyboard
+            keyboard?.keyboardInput?.keyChangedHandler = { _, _, keyCode, pressed in
+                if pressed {
+                    self.keysPressed.insert(keyCode)
+                } else {
+                    self.keysPressed.remove(keyCode)
                 }
             }
-        
+        }
+
         center.addObserver(forName: .GCMouseDidConnect,
-                           object: nil, queue: nil
-        ) {
+                           object: nil, queue: nil)
+        {
             notification in
-                let mouse = notification.object as? GCMouse
-            
+            let mouse = notification.object as? GCMouse
+
             mouse?.mouseInput?.leftButton.pressedChangedHandler =
-            {_, _, pressed in self.mousePressed = pressed}
-            
-            mouse?.mouseInput?.mouseMovedHandler = {_, deltaX, deltaY in
+                { _, _, pressed in self.mousePressed = pressed }
+
+            mouse?.mouseInput?.mouseMovedHandler = { _, deltaX, deltaY in
                 self.mouseDelta = Point2D(x: deltaX, y: deltaY)
             }
         }
